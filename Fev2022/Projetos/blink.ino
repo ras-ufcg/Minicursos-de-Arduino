2a19a4d496d1e046098635d26c4ccde0874613e4
loolirer <100168368+loolirer@users.noreply.github.com>
2022-02-23 21:29:39 -0300

    Piscando um LED com o Arduino

---
+++ Fev2022/Projetos/blink.ino
@@ -0,0 +1,13 @@
+void setup() // Só é executada uma vez, no início do programa
+{
+  pinMode(8, OUTPUT); // Define o pino 8 como saída
+}
+
+void loop() // É executada em loop
+{
+  digitalWrite(8, HIGH); // Liga o pino 8 
+  delay(1000); // Espera 1000 milissegundos (1 segundo)
+  digitalWrite(8, LOW); 
+  delay(1000); // Espera 1000 milissegundos (1 segundo)
+}
+
