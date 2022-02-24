2c3ce0ac9bbd093c4597155c5c659be2c6a531a7
loolirer <100168368+loolirer@users.noreply.github.com>
2022-02-24 14:30:50 -0300

    Comentário faltando

--- Fev2022/Projetos/blink.ino
+++ Fev2022/Projetos/blink.ino
@@ -7,7 +7,7 @@ void loop() // É executada em loop
 {
   digitalWrite(8, HIGH); // Liga o pino 8 
   delay(1000); // Espera 1000 milissegundos (1 segundo)
-  digitalWrite(8, LOW); 
+  digitalWrite(8, LOW); // Desliga o pino 8
   delay(1000); // Espera 1000 milissegundos (1 segundo)
 }
 
